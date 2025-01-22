ada0e5b00a211fe8f4e55057144e2ef957e1b306
John Doe <andreaskruszakin@gmail.com>
2025-01-22 12:53:01 +0100

    Initial Commit

---
+++ Habit Tracking App/Habit_Tracking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_Tracking_AppApp.swift
+//  Habit Tracking App
+//
+//  Created by Andreas Kruszakin-Liboska on 22/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Habit_Tracking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Habit Tracking AppTests/Habit_Tracking_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_Tracking_AppTests.swift
+//  Habit Tracking AppTests
+//
+//  Created by Andreas Kruszakin-Liboska on 22/1/25.
+//
+
+import Testing
+@testable import Habit_Tracking_App
+
+struct Habit_Tracking_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
